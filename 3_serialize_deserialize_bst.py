1fde6d3213f6fa803a25030019dee3ab32d15e34
Pop Elisa <ElisaPop@users.noreply.github.com>
2020-02-02 02:24:30 +0200

    day 4

--- 3_serialize_deserialize_bst.py
+++ 3_serialize_deserialize_bst.py
@@ -2,6 +2,8 @@
 # which serializes the tree into a string, and deserialize(s),
 # which deserializes the string back into the tree.
 
+up = 0
+
 
 class Node:
     def __init__(self, val, left=None, right=None):
@@ -10,62 +12,37 @@ class Node:
         self.right = right
 
 
-def deserialize_left(s):
-    current_element = s.partition('|')[0]
-    next_element = s.partition('|')[2]
-    if current_element is '':
-        return None
-    n = Node(current_element)
-    # skip 1
-    next_element = next_element.partition('|')[2]
-    current_element = next_element.partition('|')[0]
-    next_element = next_element.partition('|')[2]
-    n.left = deserialize(current_element)
-    n.right = deserialize(next_element.partition('|')[0])
-    return n
-
-def deserialize_right(s):
-    current_element = s.partition('|')[0]
-    next_element = s.partition('|')[2]
-    if current_element is '':
-        return None
-    n = Node(current_element)
-    # skip 2
-    next_element = next_element.partition('|')[2]
-    next_element = next_element.partition('|')[2]
-    current_element = next_element.partition('|')[0]
-    next_element = next_element.partition('|')[2]
-    n.left = deserialize(current_element)
-    n.right = deserialize(next_element.partition('|')[0])
-    return n
-
-
 def deserialize(s):
+    global up
     current_element = s.partition('|')[0]
     next_element = s.partition('|')[2]
     if current_element is '':
         return None
+    up = up + 1
     n = Node(current_element)
-    next_element = next_element.partition('|')[2]
-    n.left = deserialize_left(next_element)
-    n.right = deserialize_right(next_element.partition('|')[2])
+    n.left = deserialize(next_element)
+    for i in range(0, pow(2, up-1)):
+        print(next_element.partition('|')[0])
+        next_element = next_element.partition('|')[2]
+    n.right = deserialize(next_element.partition('|')[2])
     return n
 
 
 def serialize(root):
     serialization_string = ""
     if root is None:
-        return ""
+        return "|"
     else:
         serialization_string = serialization_string + root.val + "|"
-        serialization_string = serialization_string + serialize(root.left) + "|"
-        serialization_string = serialization_string + serialize(root.right) + "|"
+        serialization_string = serialization_string + serialize(root.left) + ""
+        serialization_string = serialization_string + serialize(root.right) + ""
 
     return serialization_string
 
 
 def main():
     node = Node('root', Node('left', Node('left.left')), Node('right'))
+    nn = deserialize(serialize(node))
     assert deserialize(serialize(node)).left.left.val == 'left.left'
 
 
